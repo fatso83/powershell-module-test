cf9229a2c479221e0fb08f7616dbcc686ae5c1fc
Carl-Erik Kopseng <carlerik@gmail.com>
2016-11-18 08:51:08 +0100

    Change module path to uppercase for unix compatibility

--- bar-test.ps1
+++ bar-test.ps1
@@ -1,6 +1,6 @@
 # set the path as the path for loading modules 
 # (I don't concatenate the existing path here to avoid x-platform differences complications)
-$env:PSModulePath = $PSScriptRoot
+$env:PSMODULEPATH = $PSScriptRoot
 
 Import-Module Bar
 
